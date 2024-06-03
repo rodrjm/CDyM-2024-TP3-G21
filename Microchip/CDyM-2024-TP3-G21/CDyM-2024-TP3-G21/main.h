da9dcc04f7a39feefd659158a6885451ea859663
20ivansandoval <20ivansandoval@gmail.com>
2024-06-03 17:55:24 -0300

    creación de la libreria para el dht11
    Se agrego la libreria para el manejo del sensor dht11 con las funciones de inicialización y de obtener datos.

---
+++ Microchip/CDyM-2024-TP3-G21/CDyM-2024-TP3-G21/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <avr/interrupt.h>
+#include <avr/io.h> // Definición de Registros del microcontrolador
+#define F_CPU 16000000UL // Frecuencia de reloj del MCU
+#include <util/delay.h> // Retardos por software – Macros: depende de F_CPU
+#include "dht11.h"
+#endif /* MAIN_H_ */
