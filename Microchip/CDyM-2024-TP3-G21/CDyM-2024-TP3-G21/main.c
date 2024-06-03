39502779854fdbb41700f5a2c4dfac08b7d82b3b
rodrjm <rodrjm94@gmail.com>
2024-06-03 17:27:43 -0300

    Lectura de datos del DHT11
    Lectura de datos del DHT11

--- Microchip/CDyM-2024-TP3-G21/CDyM-2024-TP3-G21/main.c
+++ Microchip/CDyM-2024-TP3-G21/CDyM-2024-TP3-G21/main.c
@@ -2,14 +2,15 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
+uint8_t data[5];
+
 int main(void)
 {
-	uint8_t e;
 	DDRC |= (1<<PORTC0);
 	
 	while (1)
 	{
-		PORTB|=(1<<PORTB0);
+		PORTB |= (1<<PORTB0);
 		PORTC |= (1<<PORTC0);
 		_delay_ms(18);
 		PORTC &= ~(1<<PORTC0);
@@ -17,16 +18,18 @@ int main(void)
 		PORTC |= (1<<PORTC0);
 		_delay_us(40);
 		DDRC &= ~(1<<PORTC0);
-		_delay_us(180);
-		for(int i=0;i<8;i++){
-			if (PINC &(1<<PINC0))
-			{
+		_delay_us(160);
+		for(int i=0; i<5; i++) {
+			for (int j=7; j>=0; j--) {
+				while (!((PINC &(1<<PINC0))));
 				_delay_us(30);
+				if (((PINC &(1<<PINC0)))) {
+					data[i] |= (1<<j);
+					while (((PINC &(1<<PINC0))));
+				} else {
+					data[i] |= (0<<j);
+				}
 			}
-			if (PINC &(1<<PINC0))
-			{
-				PORTB =0;
-			} else PORTB =1;
 		}
 	}
 }
