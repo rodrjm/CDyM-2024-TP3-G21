833bb959e94d56c46adb9d4e32ec5e74b478703f
rodrjm <rodrjm94@gmail.com>
2024-06-07 20:39:21 -0300

    Modificaciones varias
    Agregación de flag para controlar el RTC, y modificación de la función para obtener los datos del sensor

--- Microchip/CDyM-2024-TP3-G21/CDyM-2024-TP3-G21/dht11.c
+++ Microchip/CDyM-2024-TP3-G21/CDyM-2024-TP3-G21/dht11.c
@@ -1,5 +1,8 @@
 #include "main.h"
+
 uint8_t data[5];
+extern uint8_t RTC_flag;
+
 void inicializacion(){
 	for (int i=0;i<5;i++) data[i]=0;
 	DDRC |= (1<<PORTC0);
@@ -12,7 +15,8 @@ void inicializacion(){
 	DDRC &= ~(1<<PORTC0);
 	_delay_us(160);
 }
-void obtenerDatos(){
+
+void obtenerTemperaturaHumedad(){
 	for(int i=0; i<5; i++) {
 		for (int j=7; j>=0; j--) {
 			while (!((PINC &(1<<PINC0))));
@@ -24,5 +28,9 @@ void obtenerDatos(){
 				data[i] |= (0<<j);
 			}
 		}
+	}
+	if (data[0] + data[1] + data[2] + data[3] == data [4]) { // Verifico que se hayan mandado correctamente los datos con el checksum
+		SerialPort_TX_DHT11(data);
+		RTC_flag = 1;
 	}
 }
